626286028218e3c6e982e627e59add946b03a1c5
arieldd <arieldd@amazon.co.uk>
2022-12-19 08:37:54 +0000

    Day 19 start

---
+++ day19/solution.py
@@ -0,0 +1,15 @@
+with open("test.txt", "r") as file:
+    
+    data = [line.strip() for line in file.readlines()]
+
+    ore = 0
+    clay = 0
+    obsidian = 0
+    geodes = 0
+
+    blueprints = {}
+
+    for line in data:
+        split = line.split(':')
+        bp_id = int(split[0].split()[1])
+        cost1 = 
