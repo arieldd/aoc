cb0c2033542b40dce4bc678b95a0b550d6bf11a2
ariel <arieldd@amazon.co.uk>
2023-01-06 16:51:02 +0000

    IntCode update

--- 2019++/intcode.cpp
+++ 2019++/intcode.cpp
@@ -1,58 +1,102 @@
 #include "intcode.h"
 
-void IntCode::run_program(program_t & intcode){
+namespace IntCode{
+    void run_program(program_t & intcode){
+        int result, size;
+        for(auto it = intcode.code.begin(); it < intcode.code.end(); it += size){
+            instr_t instr = decode_instruction(it);
 
-    int result, size;
-    for(auto it = intcode.code.begin(); it < intcode.code.end(); it += size){
-        size = 1;
+            size = instr_set[instr.op];
+
+            if (instr.op == end)
+                break;
+
+            for (int i = 1; i < size; i++){
+                instr.params.push_back(*(it + i));    
+            }
+
+            execute_instruction(instr, intcode);
+        }
+    }
+
+    instr_t decode_instruction(const std::vector<int>::iterator& instr_pointer){
+        instr_t ret;
         
+        int op_val = *instr_pointer;
+        ret.op = opcode(op_val % 100);
 
-        instr_t instr = { .op = opcode(*it) };
-
-        if ( ! instr_set.count(instr.op)){
-            std::cout << "Wrong opcode" << std::endl;
-            return;
+        op_val /= 100;
+        int i = 0;
+        while(op_val){
+            ret.modes[i] = op_val%10;
+            op_val /= 10;
+            i++;
         }
 
-        size = instr_set[instr.op];
+        if ( ! instr_set.count(ret.op)){
+            std::cout << "Wrong opcode " << ret.op << std::endl;
+            ret.op = end;
+            return ret;
+        }
 
-        if (instr.op == end)
-            break;
+        int size = instr_set[ret.op];
 
         for (int i = 1; i < size; i++){
-            instr.params.push_back(*(it + i));    
+            ret.params.push_back(*(instr_pointer + i));    
         }
 
-        execute_instruction(instr, intcode);
+        return ret;
     }
-}
 
-void IntCode::execute_instruction(const instr_t& instr, program_t& intcode){
-        int op1, op2;
-        
-        switch (instr.op)
-        {
-        case add:
-            op1 = intcode.code[instr.params[0]],
-            op2 = intcode.code[instr.params[1]];
+    std::vector<int> load_instruction(const instr_t& instr, const program_t& intcode){
+        std::vector<int> instr_vals;
 
-            intcode.code[instr.params[2]] = op1 + op2;
-            break;
+        int param_count = instr_set[instr.op] - 1;
+        int param,
+            mode,
+            value;
 
-        case mult:
-            op1 = intcode.code[instr.params[0]],
-            op2 = intcode.code[instr.params[1]];
+        for (int i = 0; i < param_count; i++){
+            mode = instr.modes[i];
+            param = instr.params[i];
 
-            intcode.code[instr.params[2]] = op1 * op2;
-            break;
-        
-        default: //99 end
-            break;
+            value = (mode)? param : intcode.code[param];
+            
+            instr_vals.push_back(value);
         }
-}
 
-void IntCode::print_program(const program_t & intcode){
-    for(auto e : intcode.code)
-        std::cout << e << ",";
-    std::cout << std::endl;
-}
+        return instr_vals;
+    }
+
+    void execute_instruction(const instr_t& instr, program_t& intcode){
+            int op1, op2;
+            
+            auto instr_vals = load_instruction(instr, intcode);
+
+            switch (instr.op)
+            {
+            case add:
+                op1 = instr_vals[0];
+                op2 = instr_vals[1];
+
+                intcode.code[instr.params[2]] = op1 + op2;
+                break;
+
+            case mult:
+                op1 = instr_vals[0];
+                op2 = instr_vals[1];
+
+                intcode.code[instr.params[2]] = op1 * op2;
+                break;
+            
+            default: //99 end
+                break;
+            }
+    }
+
+    void print_program(const program_t & intcode){
+        for(auto e : intcode.code)
+            std::cout << e << ",";
+        std::cout << std::endl;
+    }
+}// namespace IntCode
--- 2019++/intcode.h
+++ 2019++/intcode.h
@@ -6,22 +6,29 @@
 
 namespace IntCode{
 
-enum opcode { add=1, mult=2, end=99 };
+enum opcode { add=1, mult=2, input=3, output=4, end=99 };
 
 struct instr_t{
     opcode op;
     std::vector<int> params;
+    std::vector<int> modes {0,0,0};
 };
 
 static std::unordered_map< opcode, int, std::hash<int> > instr_set ={
     {add, 4},
     {mult, 4},
+    {input, 2},
+    {output, 2},
     {end, 1}
 };
 
 struct program_t{
     std::vector<int> code;
 };
+
+instr_t decode_instruction(const std::vector<int>::iterator& instr_pointer);
+
+std::vector<int> load_instruction(const instr_t& instr, const program_t& intcode);
 
 void run_program(program_t & intcode);
 
