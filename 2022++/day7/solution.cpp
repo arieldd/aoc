9828e6d8120f626974202342dff71426c37a5681
Ariel Diaz De Armas <appa232@gmail.com>
2023-10-11 00:57:14 +0100

    Some adjustments on days 7 and 8.

--- 2022++/day7/solution.cpp
+++ 2022++/day7/solution.cpp
@@ -7,30 +7,137 @@
 #include <unordered_map>
 #include <vector>
 
-std::string parse_input(const std::string &file_name) {
-  std::string ret;
+using namespace std;
+
+struct FsNode {
+  string name;
+  int size;
+  FsNode *parent;
+
+  unordered_map<string, int> children;
+
+  FsNode(const string &name, int size, FsNode *parent)
+      : name(name), size(size), parent(parent) {}
+
+  FsNode() {}
+};
+
+int calculate_size(FsNode &node, vector<FsNode> &file_system) {
+  if (!node.size && node.children.size()) {
+    for (const auto &child : node.children) {
+      node.size += calculate_size(file_system[child.second], file_system);
+    }
+  }
+
+  return node.size;
+}
+
+void print(FsNode &node, vector<FsNode> &file_system, int depth) {
+  auto tabs = "";
+
+  for (int i = 0; i < depth; i++)
+    tabs += '\t';
+
+  cout << tabs + node.name << endl;
+
+  for (const auto &child : node.children) {
+    print(file_system[child.second], file_system, depth + 1);
+  }
+}
+
+std::vector<string> parse_input(const std::string &file_name) {
+  std::vector<string> ret;
 
   std::ifstream fs(file_name);
   std::string line;
   while (std::getline(fs, line)) {
-    ret = line;
+    ret.push_back(line);
   }
   return ret;
 }
 
-int part1(const std::string &data) { return 0; }
+int parse_command(const string &line, string &dir_name) {
+  if (line[0] != '$') // not a command
+    return 0;
 
-int part2(std::string data) { return 0; }
+  if (line.find("cd") != 2) // ls command
+    return 1;
+
+  dir_name = line.substr(5);
+  return 2;
+}
+
+int parse_file_data(const string &line, string &dir_name) {
+  auto middle = line.find(" ");
+
+  auto half = line.substr(0, middle);
+  dir_name = line.substr(middle + 1);
+
+  if (half == "dir") // dir listing
+    return 0;
+
+  return stoi(half);
+}
+
+void read_fs(const vector<string> &lines, vector<FsNode> &file_system) {
+  auto &current = file_system[0];
+
+  string file_name;
+
+  for (const auto &l : lines) {
+    file_name = "";
+    auto command = parse_command(l, file_name);
+    if (command) {
+      if (command < 2)
+        continue;
+
+      if (file_name == "/") {
+        current = file_system[0];
+        continue;
+      }
+
+      if (file_name == "..") {
+        if (current.parent)
+          current = *current.parent;
+        continue;
+      }
+
+      if (!current.children.count(file_name)) {
+        file_system.push_back(FsNode(file_name, 0, &current));
+        current.children[file_name] = file_system.size() - 1;
+      }
+
+      current = file_system[current.children[file_name]];
+      continue;
+    }
+
+    auto size = parse_file_data(l, file_name);
+
+    if (!current.children.count(file_name)) {
+      file_system.push_back(FsNode(file_name, size, &current));
+      current.children[file_name] = file_system.size() - 1;
+    }
+  }
+}
+
+int part1(FsNode &root) { return 0; }
+
+int part2(FsNode &root) { return 0; }
 
 int main() {
 
-  auto data = parse_input("input.txt");
-  // auto data = parse_input("test.txt");
+  // auto data = parse_input("input.txt");
+  auto data = parse_input("test.txt");
 
-  auto r1 = part1(data);
+  vector<FsNode> file_system = {FsNode("/", 0, nullptr)};
+  read_fs(data, file_system);
+
+  print(file_system[0], file_system, 0);
+
+  auto r1 = part1(file_system[0]);
   std::cout << "Part 1: " << r1 << std::endl;
 
-  auto r2 = part2(data);
+  auto r2 = part2(file_system[0]);
   std::cout << "Part 2: " << r2 << std::endl;
   return 0;
 }
--- 2022++/day8/solution.cpp
+++ 2022++/day8/solution.cpp
@@ -2,35 +2,110 @@
 #include <fstream>
 #include <functional>
 #include <iostream>
+#include <map>
 #include <numeric>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
-std::string parse_input(const std::string &file_name) {
-  std::string ret;
+std::vector<std::string> parse_input(const std::string &file_name) {
+  std::vector<std::string> ret;
 
   std::ifstream fs(file_name);
   std::string line;
   while (std::getline(fs, line)) {
-    ret = line;
+    ret.push_back(line);
+    std::cout << ret.back() << std::endl;
   }
   return ret;
 }
 
-int part1(const std::string &data) { return 0; }
+bool is_visible(std::vector<std::vector<int>> &map, int row, int column) {
+  int n = map.size(), height = map[row][column];
 
-int part2(std::string data) { return 0; }
+  int i = row - 1;
+  for (; i >= 0; i--)
+    if (map[i][column] >= height)
+      break;
+
+  if (i < 0)
+    return true;
+
+  i = row + 1;
+  for (; i < n; i++)
+    if (map[i][column] >= height)
+      break;
+
+  if (i == n)
+    return true;
+
+  i = column - 1;
+  for (; i >= 0; i--)
+    if (map[row][i] >= height)
+      break;
+
+  if (i < 0)
+    return true;
+
+  i = column + 1;
+  for (; i < n; i++)
+    if (map[row][i] >= height)
+      break;
+
+  return i == n;
+}
+
+int part1(std::vector<std::vector<int>> &map) {
+
+  int n = map.size(), visible_trees = n * 4 - 4;
+
+  for (int i = 1; i < n - 1; i++) {
+    for (int j = 1; j < n - 1; j++) {
+      if (is_visible(map, i, j)) {
+        // std::cout << map[i][j] << " (" << i << "," << j << ")" << std::endl;
+        visible_trees++;
+      }
+    }
+  }
+
+  return visible_trees;
+}
+int part2(std::vector<std::vector<int>> &map) { return 0; }
+
+std::vector<std::vector<int>> read_map(std::vector<std::string> &lines) {
+  int n = lines.size();
+
+  std::vector<std::vector<int>> result;
+
+  for (auto &line : lines) {
+    result.push_back(std::vector<int>());
+    for (auto &tree : line) {
+      int height = tree - '0';
+      result.back().push_back(height);
+    }
+  }
+
+  return result;
+}
+
+void print_map(std::vector<std::vector<int>> &map) {
+  for (auto &row : map) {
+    for (auto &entry : row) {
+      std::cout << entry << " ";
+    }
+    std::cout << std::endl;
+  }
+}
 
 int main() {
+  auto lines = parse_input("input.txt");
+  // auto lines = parse_input("test.txt");
 
-  auto data = parse_input("input.txt");
-  // auto data = parse_input("test.txt");
+  auto map = read_map(lines);
+  //print_map(map);
 
-  auto r1 = part1(data);
+  auto r1 = part1(map);
   std::cout << "Part 1: " << r1 << std::endl;
-
-  auto r2 = part2(data);
+  auto r2 = part2(map);
   std::cout << "Part 2: " << r2 << std::endl;
   return 0;
 }
