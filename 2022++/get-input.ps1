77acfa72933bd62432963909eb66d91493684f78
Ariel Diaz De Armas <appa232@gmail.com>
2023-10-13 02:16:01 +0100

    Added print utils and fixed get input script.

--- 2022++/get-input.ps1
+++ 2022++/get-input.ps1
@@ -1,1 +1,1 @@
-curl https://adventofcode.com/2022/day/9/input --cookie "session=53616c7465645f5fd59bbc621ee1d0ba25918affab1969028d7301e2abaa355e3425c112bdf8dc9e9e728074e5321d5b024bb98e8d6d6fe4913468f0bbbe5ad0" > input.txt
+curl.exe https://adventofcode.com/2022/day/9/input --cookie "session=53616c7465645f5ff253cce85a8b1d73146198711db2922bc37008ba1d8f8546e8d12b55ffde98f564f7b8f63e5381f2d31570960309b16e2af5a09257bf6cd6" > input.txt
