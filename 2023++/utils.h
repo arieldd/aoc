bd93525e1f7dbf6d910f44cb2b7ec640ef7bd7e8
Ariel Diaz De Armas <appa232@gmail.com>
2023-12-02 06:39:07 +0000

    Added split and trim functions to utils.h

--- 2023++/utils.h
+++ 2023++/utils.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 template <typename... Args> void print(Args &&...args) {
   (std::cout << ... << args);
@@ -8,4 +9,35 @@ template <typename... Args> void println(Args &&...args) {
   print(std::forward<Args>(args)...);
 
   std::cout << std::endl;
+}
+
+const char *ws = " \t\n\r\f\v";
+
+// trim from end of string (right)
+inline std::string &rtrim(std::string &s, const char *t = ws) {
+  s.erase(s.find_last_not_of(t) + 1);
+  return s;
+}
+
+// trim from beginning of string (left)
+inline std::string &ltrim(std::string &s, const char *t = ws) {
+  s.erase(0, s.find_first_not_of(t));
+  return s;
+}
+
+// trim from both ends of string (right then left)
+inline std::string &trim(std::string &s, const char *t = ws) {
+  return ltrim(rtrim(s, t), t);
+}
+
+std::vector<std::string> split(const std::string &str, char delim) {
+  std::vector<std::string> result;
+  std::stringstream ss(str);
+  std::string element;
+
+  while (getline(ss, element, delim)) {
+    result.push_back(trim(element));
+  }
+
+  return result;
 }
