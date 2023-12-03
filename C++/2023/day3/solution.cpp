542784dd3ede807806542d8a0bc6937cb3965c75
Ariel Diaz De Armas <appa232@gmail.com>
2023-12-03 08:08:03 +0000

    (2023++): Day 3 cleaner solution.

--- C++/2023/day3/solution.cpp
+++ C++/2023/day3/solution.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <map>
 #include <numeric>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -13,39 +14,47 @@ using namespace std;
 
 struct Part {
   int number;
-  int row_start;
+  int i;
+  int j;
 };
+
+const bool operator<(const Part &lhs, const Part &rhs) {
+  return (lhs.i < rhs.i) || (!(rhs.i > lhs.i) && lhs.j < rhs.j);
+}
 
 using schematic = vector<vector<Part>>;
 
 schematic parse_map(const vector<string> &lines) {
   schematic result;
 
-  for (auto &line : lines) {
+  for (int i = 0; i < lines.size(); i++) {
+    auto line = lines[i];
+
     vector<Part> row;
-    for (int i = 0; i < line.length(); i++) {
-      auto c = line[i];
+    for (int j = 0; j < line.length(); j++) {
+      auto c = line[j];
 
       if (c == '.') {
-        row.push_back({-1, 0});
+        row.push_back({-1, i, j});
+
       } else if (c >= '0' && c <= '9') {
         auto value = c - '0';
 
-        auto j = i + 1;
-        for (; j < line.length() && (line[j] >= '0' && line[j] <= '9'); j++) {
+        auto k = j + 1;
+        for (; k < line.length() && (line[k] >= '0' && line[k] <= '9'); k++) {
           value *= 10;
-          value += line[j] - '0';
+          value += line[k] - '0';
         }
 
-        for (auto k = i; k < j; k++)
-          row.push_back({value, i});
+        for (auto l = j; l < k; l++)
+          row.push_back({value, i, j});
 
-        i = j - 1;
+        j = k - 1;
 
       } else if (c == '*') {
-        row.push_back({-2, 0});
+        row.push_back({-2, i, j});
       } else {
-        row.push_back({-3, 0});
+        row.push_back({-3, i, j});
       }
     }
 
@@ -55,19 +64,7 @@ schematic parse_map(const vector<string> &lines) {
   return result;
 }
 
-void print_schematic(const schematic &map) {
-  for (auto &row : map) {
-    for (auto elem : row) {
-      if (elem.number < 0)
-        print(elem.number, " ");
-      else
-        print("(", elem.number, ",", elem.row_start, ") ");
-    }
-    println();
-  }
-}
-
-vector<string> parse_input(const string &file_name) {
+schematic parse_input(const string &file_name) {
   vector<string> ret;
 
   ifstream fs(file_name);
@@ -75,28 +72,18 @@ vector<string> parse_input(const string &file_name) {
   while (getline(fs, line)) {
     ret.push_back(line);
   }
-  return ret;
+  return parse_map(ret);
 }
 
 bool is_valid_pos(int i, int j, int r, int c) {
   return i >= 0 && j >= 0 && i < r && j < c;
 }
 
-int find_digits(int number) {
-  int result = 1;
-
-  while (number / 10) {
-    result++;
-    number /= 10;
-  }
-
-  return result;
-}
-
-const vector<int> dy{0, 0, 1, 1, 1, -1, -1, -1}, dx{-1, 1, -1, 0, 1, -1, 0, 1};
+// Start from the right clockwise
+const vector<int> dy{0, 1, 1, 1, 0, -1, -1, -1}, dx{1, 1, 0, -1, -1, -1, 0, 1};
 
 int part1(const schematic &map) {
-  vector<int> parts;
+  int total = 0;
 
   int r = map.size();
   for (int i = 0; i < r; i++) {
@@ -105,118 +92,68 @@ int part1(const schematic &map) {
     for (int j = 0; j < c; j++) {
       auto elem = row[j];
       if (elem.number <= -2) { // symbol
-        for (auto k = 0; k < 2; k++) {
+        set<Part> adjacents;
+
+        for (auto k = 0; k < 8; k++) {
           auto di = i + dy[k], dj = j + dx[k];
 
           if (is_valid_pos(di, dj, r, c) && map[di][dj].number > 0) {
             auto part = map[di][dj];
-
-            parts.push_back(part.number);
+            if (adjacents.find(part) == adjacents.end())
+              adjacents.insert(part);
           }
         }
-        for (auto k = 2; k < 5; k++) {
-          auto di = i + dy[k], dj = j + dx[k];
 
-          if (is_valid_pos(di, dj, r, c) && map[di][dj].number > 0) {
-            auto part = map[di][dj];
-
-            parts.push_back(part.number);
-            auto digits = find_digits(part.number);
-
-            auto delta = digits - (dj - part.row_start + 1);
-
-            k += delta;
-          }
-        }
-        for (auto k = 5; k < 8; k++) {
-          auto di = i + dy[k], dj = j + dx[k];
-
-          if (is_valid_pos(di, dj, r, c) && map[di][dj].number > 0) {
-            auto part = map[di][dj];
-
-            parts.push_back(part.number);
-
-            auto digits = find_digits(part.number);
-
-            auto delta = digits - (dj - part.row_start + 1);
-
-            k += delta;
-          }
+        for (auto &part : adjacents) {
+          total += part.number;
         }
       }
     }
   }
 
-  return accumulate(parts.begin(), parts.end(), 0);
+  return total;
 }
 
 int part2(const schematic &map) {
-  vector<int> ratios;
+  int total_ratios = 0;
 
   int r = map.size();
   for (int i = 0; i < r; i++) {
     auto row = map[i];
     int c = row.size();
     for (int j = 0; j < c; j++) {
-
       auto elem = row[j];
       if (elem.number == -2) { // symbol *
-        vector<int> adjacents;
+        set<Part> adjacents;
 
-        for (auto k = 0; k < 2; k++) {
+        for (auto k = 0; k < 8; k++) {
           auto di = i + dy[k], dj = j + dx[k];
 
           if (is_valid_pos(di, dj, r, c) && map[di][dj].number > 0) {
             auto part = map[di][dj];
-
-            adjacents.push_back(part.number);
-          }
-        }
-        for (auto k = 2; k < 5; k++) {
-          auto di = i + dy[k], dj = j + dx[k];
-
-          if (is_valid_pos(di, dj, r, c) && map[di][dj].number > 0) {
-            auto part = map[di][dj];
-
-            adjacents.push_back(part.number);
-            auto digits = find_digits(part.number);
-
-            auto delta = digits - (dj - part.row_start + 1);
-
-            k += delta;
-          }
-        }
-        for (auto k = 5; k < 8; k++) {
-          auto di = i + dy[k], dj = j + dx[k];
-
-          if (is_valid_pos(di, dj, r, c) && map[di][dj].number > 0) {
-            auto part = map[di][dj];
-
-            adjacents.push_back(part.number);
-
-            auto digits = find_digits(part.number);
-
-            auto delta = digits - (dj - part.row_start + 1);
-
-            k += delta;
+            if (adjacents.find(part) == adjacents.end())
+              adjacents.insert(part);
           }
         }
 
         if (adjacents.size() == 2) {
-          ratios.push_back(adjacents[0] * adjacents[1]);
+          auto ratio = 1;
+          for (auto &part : adjacents)
+            ratio *= part.number;
+
+          total_ratios += ratio;
         }
       }
     }
   }
 
-  return accumulate(ratios.begin(), ratios.end(), 0);
+  return total_ratios;
 }
 
 int main() {
-  auto lines = parse_input("input.txt");
-  // auto lines = parse_input("test.txt");
+  auto map = parse_input("input.txt");
+  // auto map = parse_input("test.txt");
 
-  auto map = parse_map(lines);
   auto r1 = part1(map);
   println("Part 1: ", r1);
   auto r2 = part2(map);
