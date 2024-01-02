d7deeb9ec559075114639583f51f78ad401c9d4e
Ariel Diaz De Armas <appa232@gmail.com>
2024-01-02 22:38:36 +0000

    (2023-rs): Day 1 better.

--- Rust/2023/day1/solution.rs
+++ Rust/2023/day1/solution.rs
@@ -7,44 +7,33 @@ fn main() {
     let input = fs::read_to_string(&args[1]).expect("Read input correctly");
     let lines = input.lines().collect::<Vec<_>>();
 
-    println!("Part 1: {}", part1(&lines));
-    println!("Part 1: {}", part2(&lines));
+    println!("Part 1: {}", solve(&lines, false));
+    println!("Part 1: {}", solve(&lines, true));
 }
 
-fn part1(lines: &[&str]) -> u32 {
-    let mut count = 0;
-
-    for l in lines {
-        let digits = l.chars().filter_map(|c| c.to_digit(10)).collect::<Vec<_>>();
-
-        let number: u32 = (digits[0] * 10 + digits[digits.len() - 1]).into();
-        count += number;
-    }
-
-    count
-}
-
-fn part2(lines: &[&str]) -> u32 {
+fn solve(lines: &[&str], part2: bool) -> u32 {
     let digit_names = [
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
 
-    let mut count = 0;
-
-    for l in lines {
-        let mut digits: Vec<u32> = vec![];
-        for (i, c) in l.char_indices() {
-            match c.to_digit(10) {
-                Some(x) => digits.push(x),
-                None => match digit_names.iter().position(|s| l[i..].starts_with(s)) {
-                    Some(pos) => digits.push((pos + 1) as u32),
-                    None => (),
-                },
+    lines
+        .iter()
+        .map(|l| {
+            let mut digits = vec![];
+            for (i, c) in l.char_indices() {
+                match c.to_digit(10) {
+                    Some(x) => digits.push(x),
+                    None => {
+                        if part2 {
+                            match digit_names.iter().position(|s| l[i..].starts_with(s)) {
+                                Some(pos) => digits.push((pos + 1) as u32),
+                                None => (),
+                            }
+                        }
+                    }
+                }
             }
-        }
-        let number = (digits[0] * 10 + digits[digits.len() - 1]) as u32;
-        count += number;
-    }
-
-    count
+            digits[0] * 10 + digits[digits.len() - 1]
+        })
+        .sum()
 }
