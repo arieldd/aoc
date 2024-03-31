2fc2359dd29b8ed0b4c64fd1047c4ab165a7c846
Ariel Diaz <appa232@gmail.com>
2024-03-31 02:00:38 +0100

    (2021-Go): Day 12 full.

--- Go/2021/day12/solution.go
+++ Go/2021/day12/solution.go
@@ -8,8 +8,6 @@ import (
 	"strings"
 )
 
-type CaveSize int
-
 type Cave struct {
 	name string
 	size int
@@ -38,6 +36,7 @@ func main() {
 
 	caves := readCaves(lines)
 	fmt.Println("Part 1: ", part1(caves))
+	fmt.Println("Part 2: ", part2(caves))
 }
 
 func part1(caves map[string]Cave) int {
@@ -45,7 +44,12 @@ func part1(caves map[string]Cave) int {
 	return findPaths(caves, path, "start", "end")
 }
 
-func findPaths(caves map[string]Cave, path []string, current string, end string) int {
+func part2(caves map[string]Cave) int {
+	path := make([]string, 0)
+	return findPaths2(caves, path, "start", "end", "start", false)
+}
+
+func findPaths(caves map[string]Cave, path []string, current, end string) int {
 	c := caves[current]
 
 	path = append(path, current)
@@ -56,6 +60,28 @@ func findPaths(caves map[string]Cave, path []string, current string, end string)
 			result++
 		} else if caves[next].size == 1 || !slices.Contains(path, next) {
 			result += findPaths(caves, path, next, end)
+		}
+	}
+
+	path = path[:len(path)-1]
+	return result
+}
+
+func findPaths2(caves map[string]Cave, path []string, current, end, start string, doubleVisit bool) int {
+	c := caves[current]
+
+	path = append(path, current)
+
+	result := 0
+	for _, next := range c.conn {
+		if next == end {
+			result++
+		} else {
+			if caves[next].size == 1 || !slices.Contains(path, next) {
+				result += findPaths2(caves, path, next, end, start, doubleVisit)
+			} else if next != start && !doubleVisit && slices.Contains(path, next) {
+				result += findPaths2(caves, path, next, end, start, true)
+			}
 		}
 	}
 
