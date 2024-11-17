c6ea14ad5ffdbe1be0b5959ec72d5e33e04350b6
Ariel Diaz <appa232@gmail.com>
2024-11-17 19:08:42 +0000

    [2020-Zig] Day 3.

--- Zig/2020/day3/solution.zig
+++ Zig/2020/day3/solution.zig
@@ -2,6 +2,11 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const split = std.mem.splitSequence;
 
+const Slope = struct {
+    right: u8,
+    down: u8,
+};
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -9,16 +14,49 @@ pub fn main() !void {
 
     const lines = try read_lines(&allocator, "input.txt");
 
-    std.debug.print("Part 1: {}\n", .{part1(lines)});
+    std.debug.print("Part 1: {}\n", .{solve(lines, .{ .right = 3, .down = 1 })});
     std.debug.print("Part 2: {}\n", .{part2(lines)});
 }
 
-fn part1(_: [][]const u8) u32 {
-    return 0;
+fn solve(lines: [][]const u8, slope: Slope) u32 {
+    const n: u32 = @intCast(lines.len);
+    const m: u32 = @intCast(lines[0].len);
+
+    var row: u32 = 0;
+    var column: u32 = 0;
+
+    var trees: u32 = 0;
+
+    while (row < n - slope.down) {
+        column += slope.right;
+        row += slope.down;
+        if (column >= m) {
+            column %= m;
+        }
+
+        if (lines[row][column] == '#') {
+            trees += 1;
+        }
+    }
+    return trees;
 }
 
-fn part2(_: [][]const u8) u32 {
-    return 0;
+fn part2(lines: [][]const u8) u32 {
+    var result: u32 = 1;
+
+    const slopes = [_]Slope{
+        .{ .right = 1, .down = 1 },
+        .{ .right = 3, .down = 1 },
+        .{ .right = 5, .down = 1 },
+        .{ .right = 7, .down = 1 },
+        .{ .right = 1, .down = 2 },
+    };
+
+    for (slopes) |slope| {
+        result *= solve(lines, slope);
+    }
+
+    return result;
 }
 
 fn read_lines(allocator: *const Allocator, filename: []const u8) ![][]const u8 {
