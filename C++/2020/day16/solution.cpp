7a22df46bfbd47e48105b6bc36cd71b915138b8e
Ariel Diaz De Armas <arieldd@amazon.co.uk>
2024-12-11 15:52:11 +0100

    [2020-C++] Day 16 ugly.

--- C++/2020/day16/solution.cpp
+++ C++/2020/day16/solution.cpp
@@ -1,9 +1,16 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iostream>
 #include <numeric>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 #define ll long long
 #define vi vector<int>
+#define umap unordered_map
 
 struct Range {
   int lower, upper;
@@ -25,28 +32,6 @@ struct Document {
   vi my_ticket;
   vector<vi> nearby_tickets;
 };
-
-ostream &operator<<(ostream &os, const Document &arg) {
-  for (auto field : arg.fields) {
-    os << field.name << ": (" << field.allowed.first.lower << ','
-       << field.allowed.first.upper << ")-(" << field.allowed.second.lower
-       << ',' << field.allowed.second.upper << ")\n";
-  }
-  os << "\nMine: ";
-  for (auto value : arg.my_ticket) {
-    os << value << ',';
-  }
-  os << '\n';
-
-  os << "\nNearby:\n";
-  for (auto &ticket : arg.nearby_tickets) {
-    for (auto value : ticket) {
-      os << value << ',';
-    }
-    os << '\n';
-  }
-  return os;
-}
 
 Document read_document(vector<string> lines) {
   auto n = lines.size();
@@ -107,9 +92,11 @@ vector<string> read_input(const string &filename) {
   return lines;
 }
 
-int part1(const Document &doc) {
-  vector<int> invalid;
-  for (auto &ticket : doc.nearby_tickets) {
+int part1(Document &doc) {
+  vi invalid_values;
+  for (int i = 0; i < doc.nearby_tickets.size(); i++) {
+    bool valid_ticket = true;
+    vi ticket = doc.nearby_tickets[i];
     for (auto value : ticket) {
       bool valid = false;
       for (auto const &field : doc.fields) {
@@ -118,19 +105,72 @@ int part1(const Document &doc) {
           break;
         }
       }
-      if (!valid)
-        invalid.push_back(value);
+      if (!valid) {
+        invalid_values.push_back(value);
+        valid_ticket = false;
+      }
+    }
+    if (!valid_ticket) {
+      doc.nearby_tickets.erase(doc.nearby_tickets.begin() + i);
+      i--;
     }
   }
-  return accumulate(invalid.begin(), invalid.end(), 0);
+  return accumulate(invalid_values.begin(), invalid_values.end(), 0);
 }
 
-int part2(const vector<string> &lines) { return 0; }
+ll part2(const Document &doc) {
+  umap<int, vi> possible;
+
+  int n = doc.nearby_tickets.size(), m = doc.my_ticket.size();
+  for (int findex = 0; findex < doc.fields.size(); findex++) {
+    auto field = doc.fields[findex];
+    possible[findex] = vi{};
+    auto &valid_pos = possible.at(findex);
+    for (int j = 0; j < m; j++) {
+      bool valid = true;
+      for (int i = 0; i < n; i++) {
+        if (!field.is_valid_value(doc.nearby_tickets[i][j])) {
+          valid = false;
+          break;
+        }
+      }
+      if (valid)
+        valid_pos.push_back(j);
+    }
+  }
+
+  vector<pair<int, vi>> list(possible.begin(), possible.end());
+  sort(list.begin(), list.end(), [](const auto e1, const auto e2) {
+    return e1.second.size() < e2.second.size();
+  });
+
+  for (int i = 0; i < list.size(); i++) {
+    auto entry = list[i];
+    assert(entry.second.size() == 1 && "Oops too many valid!");
+    auto pos = entry.second[0];
+    for (int j = i + 1; j < list.size(); j++) {
+      if (auto it = find(list[j].second.begin(), list[j].second.end(), pos);
+          it != list[j].second.end()) {
+        list[j].second.erase(it);
+      }
+    }
+  }
+
+  possible.clear();
+  possible = {list.begin(), list.end()};
+  ll result = 1;
+  for (int i = 0; i < 6; i++) {
+    auto pos = possible.at(i)[0];
+    result *= doc.my_ticket[pos];
+  }
+  return result;
+}
 
 int main(int argc, char *argv[]) {
   assert(argc > 1 && "Need some input brotha\n");
   auto lines = read_input(argv[1]);
   auto doc = read_document(lines);
   cout << "Part 1:" << part1(doc) << '\n';
+  cout << "Part 2:" << part2(doc) << '\n';
   return 0;
 }
