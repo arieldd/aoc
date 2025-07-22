6d98cf4f0d3efa967423bc784f1bced908dd821a
Ariel Diaz <appa232@gmail.com>
2025-07-22 21:11:43 +0100

    [2017-Ruby] Day 1

---
+++ Ruby/2017/day1/main.rb
@@ -0,0 +1,20 @@
+input = File.open("input.txt")
+
+def captcha(text, step)
+  last = text.size - 1
+
+  sum = 0
+  for i in 0..last do
+    j = (i + step) % last
+    d1 = text[i].to_i
+    d2 = text[j].to_i
+    sum += d1 == d2 ? d1 : 0
+  end
+
+  sum
+end
+
+input.each do |line|
+  puts captcha(line, 1)
+  puts captcha(line, line.size / 2)
+end
