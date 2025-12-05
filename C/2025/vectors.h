8a0bd3c759330eec9e84199731143ac92d3261ff
Ariel Diaz De Armas <8loop2@gmail.com>
2025-12-05 08:10:26 +0000

    [C] Refactor vector types.
    - Now receives a custom comparator function for `remove`
    - Added `remove_at`
    - Removed `at` function as it was not really needed

--- C/2025/vectors.h
+++ C/2025/vectors.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdlib.h>
 
-#define DECLARE_VECTOR_TYPE(item_type, type_name)                              \
+#define DECLARE_VECTOR_TYPE(item_type, type_name, cmp)                         \
   typedef struct {                                                             \
     item_type *items;                                                          \
     size_t count;                                                              \
@@ -25,12 +25,6 @@
     return 0;                                                                  \
   }                                                                            \
                                                                                \
-  item_type type_name##_at(type_name *arr, size_t idx) {                       \
-    if (idx >= arr->count)                                                     \
-      return 0;                                                                \
-    return arr->items[idx];                                                    \
-  }                                                                            \
-                                                                               \
   int type_name##_append(type_name *arr, item_type element) {                  \
     if (arr->count >= arr->capacity) {                                         \
       void *new_mem =                                                          \
@@ -44,10 +38,24 @@
     return arr->count;                                                         \
   }                                                                            \
                                                                                \
+  int type_name##_remove_at(type_name *arr, int index) {                       \
+    if (index < 0 || index >= arr->count)                                      \
+      return -1;                                                               \
+    arr->items[index] = arr->items[arr->count - 1];                            \
+    arr->count--;                                                              \
+    if (arr->count < arr->capacity / 4) {                                      \
+      void *new_mem =                                                          \
+          realloc(arr->items, sizeof(item_type) * arr->capacity / 2);          \
+      arr->items = (item_type *)new_mem;                                       \
+      arr->capacity /= 2;                                                      \
+    }                                                                          \
+    return 0;                                                                  \
+  }                                                                            \
+                                                                               \
   int type_name##_remove(type_name *arr, item_type element) {                  \
     int i = 0;                                                                 \
     for (i = 0; i < arr->count; i++) {                                         \
-      if (arr->items[i] == element) {                                          \
+      if (cmp(arr->items[i], element)) {                                       \
         arr->items[i] = arr->items[arr->count - 1];                            \
         break;                                                                 \
       }                                                                        \
@@ -64,10 +72,20 @@
     return 0;                                                                  \
   }
 
+#define EQ_CMP(type, name)                                                     \
+  char cmp_##name(type e1, type e2) { return e1 == e2; };
+
+EQ_CMP(int, i);
+EQ_CMP(long, l);
+EQ_CMP(long long, ll);
+EQ_CMP(float, f);
+EQ_CMP(double, d);
+EQ_CMP(char, c);
+
 // Basic vector types
-DECLARE_VECTOR_TYPE(int, vi)
-DECLARE_VECTOR_TYPE(long, vl)
-DECLARE_VECTOR_TYPE(long long, vll)
-DECLARE_VECTOR_TYPE(float, vf)
-DECLARE_VECTOR_TYPE(double, vd)
-DECLARE_VECTOR_TYPE(char, str)
+DECLARE_VECTOR_TYPE(int, vi, cmp_i)
+DECLARE_VECTOR_TYPE(long, vl, cmp_l)
+DECLARE_VECTOR_TYPE(long long, vll, cmp_ll)
+DECLARE_VECTOR_TYPE(float, vf, cmp_f)
+DECLARE_VECTOR_TYPE(double, vd, cmp_d)
+DECLARE_VECTOR_TYPE(char, str, cmp_c)
